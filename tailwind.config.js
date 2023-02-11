90ad7bb3c51817aba3824975adde3f5b8a4e6b8c
Jo SeongHwan <jamonggg@gmail.com>
2023-02-11 16:24:04 +0900

    tailwindCss를 사용하여 테스트 썸네일 출력

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [ "./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      fontSize:{
+        xs:['12px',{lineHeight:'18px'}],
+        sm:['14px',{lineHeight: '22px'}],
+        base:['16px',{lineHeight:'22px'}],
+        lg:['18px',{lineHeight:'24px'}],
+        xl:['28px',{lineHeight:'40px'}],
+      }
+    },
+  },
+  plugins: [],
+}
+ 

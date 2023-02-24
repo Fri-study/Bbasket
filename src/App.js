1ed3d2af82e74ec5f3aa5d3fe76b7bc9a5e2ae52
chaedayes <dusvlf5b@gmail.com>
2023-02-24 17:37:14 +0900

    json 정리 확인 테스트

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddBookMark from "./components/AddBookMark";
 import Login from "./components/Login/Login";
@@ -6,17 +7,17 @@ import Home from "./components/Pages/Home";
 
 function App() {
   //메인프레임 선택에 따른 메인 상태변화
-  const [showState, setShowState] = useState('main');
+  const [showState, setShowState] = useState("main");
   //const [showState, setShowState] = useState('usermain');
 
   //로그인 => user메인페이지
   const handleLogin = () => {
-    setShowState('usermain')
-  }
+    setShowState("usermain");
+  };
   //로그아웃 => 메인페이지
   const handleLogout = () => {
-    setShowState('main')
-  }
+    setShowState("main");
+  };
 
   //ex) <frameComponent onlogin={() => {handleLogin}}>
 

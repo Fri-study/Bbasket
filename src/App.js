75bb7c616fffc7b3d0db41c58b8827850055a060
chaedayes <dusvlf5b@gmail.com>
2023-02-25 00:40:07 +0900

    [Style] 태그 수정

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import AddBookMark from "./components/AddBookMark";
 import Login from "./components/Login/Login";
@@ -6,21 +5,6 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Pages/Home";
 
 function App() {
-  //메인프레임 선택에 따른 메인 상태변화
-  const [showState, setShowState] = useState("main");
-  //const [showState, setShowState] = useState('usermain');
-
-  //로그인 => user메인페이지
-  const handleLogin = () => {
-    setShowState("usermain");
-  };
-  //로그아웃 => 메인페이지
-  const handleLogout = () => {
-    setShowState("main");
-  };
-
-  //ex) <frameComponent onlogin={() => {handleLogin}}>
-
   return (
     <div>
       <BrowserRouter>
